f34e5917c960e2047089ddb160c970eb305bfbf4
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-11-10 19:47:17 +0100

    Account CRUD - CREATE Account

--- Bank/bank_api.js
+++ Bank/bank_api.js
@@ -23,12 +23,12 @@ db.run(`CREATE TABLE IF NOT EXISTS BankUser(
 db.run(`CREATE TABLE IF NOT EXISTS Account(
     Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     BankUserId INTEGER NOT NULL,
-    AccountNo TEXT NOT NULL,
+    AccountNo TEXT UNIQUE NOT NULL,
     IsStudent INTEGER,
     CreatedAt DATE DEFAULT (datetime('now','localtime')),
     ModifiedAt DATE DEFAULT (datetime('now','localtime')),
-    InterestRate INTEGER NOT NULL,
-    Amount INTEGER NOT NULL,
+    InterestRate REAL NOT NULL,
+    Amount REAL NOT NULL,
     FOREIGN KEY(BankUserId) REFERENCES BankUser(Id))`
 );
 
@@ -36,7 +36,7 @@ db.run(`CREATE TABLE IF NOT EXISTS Deposit(
     Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     BankUserId INTEGER NOT NULL,
     CreatedAt DATE DEFAULT (datetime('now','localtime')),
-    Amount INTEGER NOT NULL,
+    Amount REAL NOT NULL,
     FOREIGN KEY(BankUserId) REFERENCES BankUser(Id))`
 );
 
@@ -45,7 +45,7 @@ db.run(`CREATE TABLE IF NOT EXISTS Loan(
     BankUserId INTEGER NOT NULL,
     CreatedAt DATE DEFAULT (datetime('now','localtime')),
     ModifiedAt DATE DEFAULT (datetime('now','localtime')),
-    Amount INTEGER NOT NULL,
+    Amount REAL NOT NULL,
     FOREIGN KEY(BankUserId) REFERENCES BankUser(Id))`
 );
 
@@ -191,24 +191,155 @@ app.delete("/bank/:id", (req, res) => {
 // |   Account API   |
 // -------------------
 
-// app.post("/account", (req, res) => {
-//     let bankUserId = req.body.userId;
-//     let sql = `INSERT INTO Account(BankUserId, AccountNo, isStudent) VALUES(?)`;
+// CREATE Account
+app.post("/account", (req, res) => {
+    let bankUserId = req.body.bankUserId;
+    let accountNo = req.body.accountNo;
+    let isStudent = req.body.isStudent;
+    let interestRate = req.body.interestRate;
+    let amount = req.body.amount;
+    let sqlAccount = `INSERT INTO Account(BankUserId, AccountNo, IsStudent, InterestRate, Amount) VALUES(?, ?, ?, ?, ?)`;
+
+    db.run(sqlAccount, [bankUserId, accountNo, isStudent, interestRate, amount], (err) => {
+        if (err) {
+            if(err.message === 'SQLITE_CONSTRAINT: UNIQUE constraint failed: Account.AccountNo') {
+                res.status(400).json({
+                    message: 'The Account Number already exists!',
+                    error: err.message
+                });
+            }
+            res.status(400).json({
+                message: 'The Account could not be created!',
+                error: err.message
+            });
+            console.log(err.message);
+        }
+        console.log(`A new row has been inserted!`);
+        res.status(201).json({
+            message: 'Account successfully created!',
+        });
+    });
+});
 //
-//     db.run(sql, [bankUserId], (err) => {
+// // READ Accounts
+// app.get("/bank", (req, res) => {
+//     let sql = `SELECT * FROM BankUser`;
+//     db.all(sql, [], (err, bankUsers) => {
 //         if (err) {
 //             res.status(400).json({
-//                 message: 'The bank user could not be created!',
-//                 error: err.message
+//                 message: 'The bank users could not be showed!',
+//                 error: err
 //             });
-//             console.log(err.message);
+//             console.log(err);
 //         }
-//         console.log(`A new row has been inserted!`);
-//         res.status(201).json({
-//             message: 'Bank user successfully created!',
+//
+//         res.status(200).json({
+//             bankUsers
 //         });
 //     });
 // });
+//
+// // READ Account by Id
+// app.get("/bank/:id", (req, res) => {
+//     console.log("req.params.id: ", req.params.id);
+//     let sql = `SELECT * FROM BankUser WHERE Id = ?`;
+//
+//     db.all(sql, [req.params.id], (err, bankUser) => {
+//         if (err) {
+//             res.status(400).json({
+//                 error: err
+//             });
+//             console.log(err);
+//         }
+//         if(bankUser.length) {
+//             res.status(200).json({
+//                 bankUser
+//             });
+//         } else {
+//             res.status(404).json({
+//                 message: `No bank user found with the id ${req.params.id}!`
+//             });
+//         }
+//     });
+// });
+//
+// // UPDATE Account by Id
+// app.put("/bank/:id", (req, res) => {
+//     console.log("req.params.id: ", req.params.id);
+//     let bankUserId = req.body.userId;
+//     let sqlGet = `SELECT * FROM BankUser WHERE Id = ?`;
+//     let sqlUpdate = `UPDATE BankUser SET UserId = ?, ModifiedAt = ? WHERE Id = ?`;
+//     db.all(sqlGet, [req.params.id], (err, bankUser) => {
+//         if (err) {
+//             res.status(400).json({
+//                 error: err
+//             });
+//             console.log(err);
+//         }
+//         console.log("Bank User: ", bankUser);
+//         if(!bankUser.length) {
+//             res.status(404).json({
+//                 message: `No bank user found with the id ${req.params.id}!`
+//             });
+//         } else {
+//             let date = new Date();
+//             let year = date.getFullYear();
+//             let month = ("0" + (date.getMonth() + 1)).slice(-2);
+//             let day = ("0" + date.getDate()).slice(-2);
+//             let hours = ("0" + date.getHours()).slice(-2);
+//             let minutes = ("0" + date.getMinutes()).slice(-2);
+//             let seconds = ("0" + date.getSeconds()).slice(-2);
+//             let modifiedAt = year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
+//             db.run(sqlUpdate, [bankUserId, modifiedAt, req.params.id], (err) => {
+//                 if (err) {
+//                     res.status(400).json({
+//                         message: 'The bank user could not be updated!',
+//                         error: err.message
+//                     });
+//                     console.log(err.message);
+//                 }
+//                 res.status(201).json({
+//                     message: 'Bank user successfully updated!',
+//                 });
+//             });
+//         }
+//     });
+// });
+//
+// // DELETE Account by Id
+// app.delete("/bank/:id", (req, res) => {
+//     console.log("req.params.id: ", req.params.id);
+//     let sqlGet = `SELECT * FROM BankUser WHERE Id = ?`;
+//     let sqlDelete = `DELETE FROM BankUser WHERE Id = ?`;
+//     db.all(sqlGet, [req.params.id], (err, bankUser) => {
+//         if (err) {
+//             res.status(400).json({
+//                 error: err
+//             });
+//             console.log(err);
+//         }
+//         console.log("Bank user: ", bankUser);
+//         if(!bankUser.length) {
+//             res.status(404).json({
+//                 message: `No bank user found with the id ${req.params.id}!`
+//             });
+//         } else {
+//             db.run(sqlDelete, req.params.id, (err) => {
+//                 if (err) {
+//                     res.status(400).json({
+//                         message: 'The bank user could not be deleted!',
+//                         error: err.message
+//                     });
+//                     console.log(err.message);
+//                 }
+//                 res.status(201).json({
+//                     message: 'Bank user successfully deleted!'
+//                 });
+//             });
+//         }
+//     });
+// });
+
 
 app.listen(PORT, HOSTNAME, (err) => {
     if(err){
