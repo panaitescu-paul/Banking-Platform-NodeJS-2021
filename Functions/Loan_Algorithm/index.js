6feddf2a4fe1a891c12cc4b078cd36ff80b23f1b
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-11-10 21:54:31 +0100

    Loan_Algorithm Function

---
+++ Functions/Loan_Algorithm/index.js
@@ -0,0 +1,15 @@
+module.exports = async function (context, req) {
+
+    const loan = (req.query.loan || (req.body && req.body.loan));
+    const totalAccountAmount = (req.query.totalAccountAmount || (req.body && req.body.totalAccountAmount));
+
+    if (loan > totalAccountAmount * 0.75) {
+        context.res = {
+            status: 403
+        };
+    } else {
+        context.res = {
+            status: 200
+        };
+    }
+}
