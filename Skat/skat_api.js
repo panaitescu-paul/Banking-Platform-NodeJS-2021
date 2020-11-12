28d3ff21ec0ee39de64fc58a9607d1e74937d70c
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-11-12 20:01:12 +0100

    added endpoint: CREATE Skat Year (/skat-year)

--- Skat/skat_api.js
+++ Skat/skat_api.js
@@ -179,6 +179,171 @@ app.delete("/skat-user/:id", (req, res) => {
     });
 });
 
+// -------------------
+// |   Skat Year API   |
+// -------------------
+
+// CREATE Skat Year
+app.post("/skat-year", (req, res) => {
+    let label = req.body.label;
+    let startDate = req.body.startDate;
+    let endDate = req.body.endDate;
+    let skatYearId;
+    let sqlSkatYear = `INSERT INTO SkatYear(Label, StartDate, EndDate) VALUES(?, ? , ?)`;
+    let sqlGet = `SELECT * FROM SkatUser`;
+    let sqlSkatUserYear = `INSERT INTO SkatUserYear(SkatUserId, SkatYearId, UserId, Amount) VALUES(?, ? , ?, ?)`;
+
+    db.run(sqlSkatYear, [label, startDate, endDate], function(err) {
+        if (err) {
+            res.status(400).json({
+                message: 'The Skat Year could not be created!',
+                error: err.message
+            });
+            console.log(err.message);
+        }
+        console.log(`A new row has been inserted!`);
+        skatYearId = this.lastID; // get the id of the record
+        console.warn("inserted id:", this.lastID);
+        db.all(sqlGet, [], (err, users) => {
+            if (err) {
+                res.status(400).json({
+                    message: 'The Skat Users could not be showed!',
+                    error: err
+                });
+                console.log(err);
+            }
+            for (i = 0; i < users.length; i++) {
+                // Add record in the Skat User Year Table for each Skat User
+                console.log("users[i].Id" + users[i].Id);
+                db.run(sqlSkatUserYear, [users[i].Id, skatYearId, users[i].UserId, 0], (err) => {
+                    if (err) {
+                        res.status(400).json({
+                            message: 'The Skat User Year could not be created!',
+                            error: err.message
+                        });
+                        console.log(err.message);
+                    }
+                    console.log(`A new Skat User Year has been inserted!`);
+                });
+            }
+        });
+        res.status(200).json({
+            message: 'Skat Year successfully created!'
+        });
+    });
+});
+
+// // READ ALL Skat Users
+// app.get("/skat-user", (req, res) => {
+//     let sql = `SELECT * FROM SkatUser`;
+//     db.all(sql, [], (err, users) => {
+//         if (err) {
+//             res.status(400).json({
+//                 message: 'The Skat Users could not be showed!',
+//                 error: err
+//             });
+//             console.log(err);
+//         }
+//
+//         res.status(200).json({
+//             users
+//         });
+//     });
+// });
+//
+// // READ Skat User by userId
+// app.get("/skat-user/:userId", (req, res) => {
+//     console.log("req.params.userId: ", req.params.userId);
+//     let sql = `SELECT * FROM SkatUser WHERE UserId = ?`;
+//
+//     db.all(sql, [req.params.userId], (err, users) => {
+//         if (err) {
+//             res.status(400).json({
+//                 error: err
+//             });
+//             console.log(err);
+//         }
+//         if(users.length) {
+//             res.status(200).json({
+//                 users
+//             });
+//         } else {
+//             res.status(404).json({
+//                 message: `No Skat User was found with the userId ${req.params.userId}!`
+//             });
+//         }
+//     });
+// });
+//
+// // UPDATE Account by Id
+// app.put("/skat-user/:id", (req, res) => {
+//     let userId = req.body.userId;
+//     let isActive = req.body.isActive;
+//     let sqlGet = `SELECT * FROM SkatUser WHERE Id = ?`;
+//     let sqlUpdate = `UPDATE SkatUser SET UserId = ?, IsActive = ? WHERE Id = ?`;
+//     db.all(sqlGet, [req.params.id], (err, skatUser) => {
+//         if (err) {
+//             res.status(400).json({
+//                 error: err
+//             });
+//             console.log(err);
+//         }
+//         console.log("Skat User: ", skatUser);
+//         if(!skatUser.length) {
+//             res.status(404).json({
+//                 message: `No Skat User found with the id ${req.params.id}!`
+//             });
+//         } else {
+//             db.run(sqlUpdate, [userId, isActive, req.params.id], (err) => {
+//                 if (err) {
+//                     res.status(400).json({
+//                         message: 'The Skat User could not be updated!',
+//                         error: err.message
+//                     });
+//                     console.log(err.message);
+//                 }
+//                 res.status(201).json({
+//                     message: 'Skat user successfully updated!',
+//                 });
+//             });
+//         }
+//     });
+// });
+//
+// // DELETE Skat User by Id
+// app.delete("/skat-user/:id", (req, res) => {
+//     console.log("req.params.id: ", req.params.id);
+//     let sqlGet = `SELECT * FROM SkatUser WHERE Id = ?`;
+//     let sqlDelete = `DELETE FROM SkatUser WHERE Id = ?`;
+//     db.all(sqlGet, [req.params.id], (err, user) => {
+//         if (err) {
+//             res.status(400).json({
+//                 error: err
+//             });
+//             console.log(err);
+//         }
+//         console.log("User: ", user);
+//         if(!user.length) {
+//             res.status(404).json({
+//                 message: `No Skat User was found with the id ${req.params.id}!`
+//             });
+//         } else {
+//             db.run(sqlDelete, req.params.id, (err) => {
+//                 if (err) {
+//                     res.status(400).json({
+//                         message: 'The Skat User could not be deleted!',
+//                         error: err.message
+//                     });
+//                     console.log(err.message);
+//                 }
+//                 res.status(200).json({
+//                     message: 'Skat User successfully deleted!'
+//                 });
+//             });
+//         }
+//     });
+// });
+
 app.listen(PORT, HOSTNAME, (err) => {
     if(err){
         console.log(err);
